67daa45707fe3a95d882d4720802c896901a6e6e
Yusupov-droid <jamnum9813@gmail.com>
2021-06-11 12:19:11 -0500

    Add new Solution

---
+++ JavaScript/4KYU/SumStringsAsNumbers.js
@@ -0,0 +1,40 @@
+function sumStrings(a = '', b = '') {
+    let result = []
+    let inTheMind = 0;
+
+    if (Number(a) < Number(b)) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+
+    // Trim number started with zeros
+    a =  a.replace(/^0+/gi , '')
+    b =  b.replace(/^0+/gi , '')
+
+    console.log(a , '\n===================\n' , b);
+
+    for (let i = 0; i < a.length; i++) {
+        let num_one = Number(a[a.length - 1 - i]) 
+        let num_two = Number(b[b.length - 1 - i]) || 0
+        
+
+
+        let temp = num_one + num_two + inTheMind
+
+        if (temp >= 10) {
+            inTheMind = 1;
+            result.push(temp - 10)
+        }
+        else {
+            inTheMind = 0;
+
+            result.push(temp)
+        }
+    }
+    if (inTheMind > 0) result.push(inTheMind)
+    return result.reverse().join('')
+}
+
+
+console.log(sumStrings('00103', '08567'));
